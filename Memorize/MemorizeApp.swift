0e7f1898f5afc3fea2fab69676edcf54054fd716
Xiaofei Ge <xfge.cn@gmail.com>
2021-08-18 21:26:37 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 葛笑非 on 2021/8/18.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

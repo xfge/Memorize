45457342db49641780cdae54dac30765a3f4b230
Xiaofei Ge <xfge.cn@gmail.com>
2021-08-29 11:38:33 +0800

    Lecture 7: ViewModifier Animation

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -32,31 +32,26 @@ struct CardView: View {
     
     var body: some View {
         GeometryReader(content: { geometry in
-            let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
             ZStack {
-                if card.isFaceUp {
-                    shape.fill().foregroundColor(.white)
-                    shape.stroke(lineWidth: DrawingConstants.lineWidth)
-                    Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 110-90))
-                        .padding(DrawingConstants.piePadding)
-                        .opacity(DrawingConstants.pieOpacity)
-                    Text(card.content).font(font(in: geometry.size))
-                } else if card.isMatched {
-                    shape.opacity(0)
-                } else {
-                    shape.fill()
-                }
+                Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 110-90))
+                    .padding(DrawingConstants.piePadding)
+                    .opacity(DrawingConstants.pieOpacity)
+                Text(card.content)
+                    .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
+                    .animation(.linear(duration: 1).repeatForever(autoreverses: false))
+                    .font(Font.system(size: DrawingConstants.fontSize))
+                    .scaleEffect(scale(thatFits: geometry.size))
             }
+            .cardify(isFaceUp: card.isFaceUp)
         })
     }
     
-    private func font(in size: CGSize) -> Font {
-        Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
+    private func scale(thatFits size: CGSize) -> CGFloat {
+        min(size.width, size.height) * DrawingConstants.fontScale / DrawingConstants.fontSize
     }
     
     private struct DrawingConstants {
-        static let cornerRadius: CGFloat = 10
-        static let lineWidth: CGFloat = 3
+        static let fontSize: CGFloat = 32
         static let fontScale: CGFloat = 0.7
         static let piePadding: CGFloat = 5
         static let pieOpacity: Double = 0.5
